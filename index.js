57821828db4868b5762c7c4415424358ab9956dc
Abisekh .G <abisekh1902@gmail.com>
2026-03-26 21:35:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./Config/db.js";
+import userRouter from "./routes/user.route.js";
+import bookRouter from "./routes/book.route.js";
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  connectDb();
+  console.log(`Our server is working at PORT :${PORT}`);
+});
+
+app.use("/api/authuser", userRouter); // Move this up
+app.use("/api/book", bookRouter);     // Move this up
+
+app.listen(PORT, () => {
+    connectDb();
+    console.log(`Our server is working at PORT: ${PORT}`);
+});
